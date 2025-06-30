3c8edd1d1bc75215ef2ecb3f04ce0898dbcbe4e7
goldoneen <goldone0101@gmail.com>
2025-06-30 12:51:36 -0700

    feat: add functionalities and UI to login, logout and change/reset pw

---
+++ account/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for bookmarks project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # path('login/', views.user_login, name='login'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # # reset password urls
+    # path(
+    #     'password-reset/',
+    #     auth_views.PasswordResetView.as_view(),
+    #     name='password_reset'
+    # ),
+    # path(
+    #     'password-reset/done/',
+    #     auth_views.PasswordResetDoneView.as_view(),
+    #     name='password_reset_done'
+    # ),
+    # path(
+    #     'password-reset/<uidb64>/<token>/',
+    #     auth_views.PasswordResetConfirmView.as_view(),
+    #     name='password_reset_confirm'
+    # ),
+    # path('password-reset/complete/',
+    #     auth_views.PasswordResetCompleteView.as_view(),
+    #     name='password_reset_complete'
+    # ),
+]

29a46cd470a21666b011238d326ab764a126a782
goldoneen <goldone0101@gmail.com>
2025-07-01 06:39:11 -0700

    feat: add functionality and UI for registeration

--- account/urls.py
+++ account/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     # path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
     # path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     # path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     # # reset password urls
